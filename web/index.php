be1f910e51e6eba33a0519acb6c7471da0094349
vanadiaaviandi <50947568+vanadiaaviandi@users.noreply.github.com>
2019-10-13 10:00:23 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,25 +1,7 @@
-<?php
+<html>
+  
+  <h2>NIM : 10116287</h2>
+  <h2>NAMA : Vanadia Aviandi </h2>
+  
+</html>
 
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
